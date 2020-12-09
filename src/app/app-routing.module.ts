f553544ae210639b984607e0274f89afa0a583e6
Georgi Chochev <georgi.chochev@Georgi-Chochev.local>
2020-12-09 23:32:12 +0200

    new crypto-list/:id route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { CryptoListComponent } from './crypto-list/crypto-list.component';
+import { DetailsComponent } from './coin-details/details.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
@@ -11,8 +12,13 @@ const routes: Routes = [
   },
   {
     path: 'crypto-list',
-    component: CryptoListComponent
-  }
+    component: CryptoListComponent,
+  },
+  {
+    path: 'crypto-list/:id',
+    component: DetailsComponent,
+  },
+
 ];
 
 @NgModule({
