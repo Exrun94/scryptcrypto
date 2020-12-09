1597eec3664e959b9dd1da1fa2335ede08bbea81
Georgi Chochev <georgi.chochev@Georgi-Chochev.local>
2020-12-09 04:21:26 +0200

    Updated API params query

--- src/app/services/coins.service.ts
+++ src/app/services/coins.service.ts
@@ -12,6 +12,6 @@ export class CoinsService {
   constructor(private httpClient: HttpClient) { }
 
   public getData(): Observable<ICoins[]> {
-    return this.httpClient.get<ICoins[]>(`${ENV.baseUrl}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`)
+    return this.httpClient.get<ICoins[]>(`${ENV.baseUrl}coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d`)
   }
 }
